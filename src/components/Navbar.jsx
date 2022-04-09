cab7c424293d6f23a2bdc60f3e56d8183558f497
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-08 22:23:41 -0400

    fully responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,28 @@
 import '../App.css'
+import { useState } from "react";
+import ReorderIcon from '@material-ui/icons/Reorder'
+import SearchIcon from '@material-ui/icons/Search'
 
 
 function Navbar() {
+
+    const [showLinks, setShowLinks] = useState(false)
+
     return(
         <div className="navbar">
             <div className="leftSide">
-                <div className="links">
+                <div className="links" id={showLinks ? "hidden" : ""}>
                     <a href="/home">Home</a>
                     <a href="/feedback">Feedback</a>
                     <a href="/aboutus">About Us</a>
                     <a href="/contact">Contact</a>
                 </div>
+                <button onClick={() => setShowLinks(!showLinks)}> <ReorderIcon/> </button>
             </div>
 
             <div className="rightSide">
                 <input type="text" placeholder="Search..." />
-                <button>Search</button>
+                <button> <SearchIcon /> </button>
             </div>
         </div>
     )
