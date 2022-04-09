a153fe8c2a56fb50c38b72e7e918ccfe4b4899e3
Steven Mikhu <StevenMikhu@gmail.com>
2022-04-08 21:52:20 -0400

    navbar links added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import '../App.css'
+
+
+function Navbar() {
+    return(
+        <div className="navbar">
+            <div className="leftSide">
+                <div className="links">
+                    <a href="/home">Home</a>
+                    <a href="/feedback">Feedback</a>
+                    <a href="/aboutus">About Us</a>
+                    <a href="/contact">Contact</a>
+                </div>
+            </div>
+
+            <div className="rightSide">
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
